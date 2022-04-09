9e1ae8027af4f243a4b9de4d4d71120952705261
minh son do <minh-son.do@students.mq.edu.au>
2022-04-09 12:11:33 +1000

    A new start screen

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Button() {
+  return (
+    <div className="App">
+     <button>Start </button>
+     <button>Admin </button>
+
+    </div>
+  );
+}
+
+export default Button;
