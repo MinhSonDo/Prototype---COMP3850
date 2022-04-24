9b587b633401e3d5fec116845493388a9de48292
minh son do <minh-son.do@students.mq.edu.au>
2022-04-24 16:15:25 +1000

    Created a wheel spinner

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-
+import Wheel from './components/wheel';
 
 
 const setClick1 =()=> {
@@ -19,20 +19,23 @@ const setClick3 =()=> {
 
 }
 
-
+const places = ['Set1', 'Set2', 'Set3'];
 
 function Menu() {
   return (
-
+<div>
     <div class="buttons has-addons is-centered">
      <button class="button" onClick={setClick1}> Set 1</button>
      <button class="button" onClick={setClick2}> Set 2</button>
      <button class="button"  onClick={setClick3}> Set 3</button>
 
-
-
+   
+     
     </div>
-    
+    <p>Randomizer</p>
+  
+    <Wheel items={places} />
+    </div> 
          
   );
 }
