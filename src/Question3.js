5fbf062d400b9f2cf51b6e31479723468a69e904
minh son do <minh-son.do@students.mq.edu.au>
2022-05-01 14:16:42 +1000

    Apply the changes to Set2 and Set3

--- src/Question3.js
+++ src/Question3.js
@@ -4,15 +4,32 @@ import QuestionSet3 from './QuestionSet3.json';
 import React, { useState } from 'react';
 import { Rating } from 'react-simple-star-rating'
 
+function generateRandom(min = 0, max = QuestionSet3.length) {
+
+    // find diff
+    let difference = max - min;
+
+    // generate random number 
+    let rand = Math.random();
+
+    // multiply with difference 
+    rand = Math.floor( rand * difference);
+
+    // add with min value 
+    rand = rand + min;
+
+    return rand;
+}
 
 
 
 
+function Question3() {
 
-function Question3(props) {
 
+     let temp = generateRandom();
 
-     let temp = props.temp;
+     console.log(temp);
  
         return (
 
@@ -21,15 +38,15 @@ function Question3(props) {
                 <p class="title"> {QuestionSet3[temp].question}</p>
                 <div class="buttons is-centered">
                     <p><button class="button" >{QuestionSet3[temp].answers[0]}   </button>    </p>
-                    <p> <button class="button" >{QuestionSet3[temp].answers[1]} </button>     </p>
+                    <p> <button class="button" >{QuestionSet3[temp].answers[3]} </button>     </p>
                     <p> <button class="button" > {QuestionSet3[temp].answers[2]} </button>     </p>
                     <p><button class="button" > {QuestionSet3[temp].answers[3]}   </button>   </p>
                 </div>
 
-                <div class="buttons is-centered">
+                {/* <div class="buttons is-centered">
                     <button class="button is-success is-light" > Previous</button>
 
-                </div>
+                </div> */}
 
             </div>
 
