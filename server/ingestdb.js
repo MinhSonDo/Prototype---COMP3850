af8f61d6fa6f970195fda91855558eae00dc6891
minh son do <minh-son.do@students.mq.edu.au>
2022-05-08 13:33:15 +1000

    added Set1, Set2 and Set3 to Mongodb

--- server/ingestdb.js
+++ server/ingestdb.js
@@ -1,10 +1,19 @@
 require("dotenv").config()
 const mongoose = require("mongoose")
 const Admins = require("./models/admins")
+const Set1 = require("./models/Set1")
+// const Set2 = require("./models/Set2")
+// const Set3 = require("./models/Set3")
 const fs = require("fs")
 
 const rawAdminsData = fs.readFileSync("server/adminData.json")
 const adminsData = JSON.parse(rawAdminsData)
+const rawSet1Data = fs.readFileSync("server/Set1.json")
+const set1Data = JSON.parse(rawSet1Data)
+// const rawSet2Data = fs.readFileSync("server/Set2.json")
+// const set2Data = JSON.parse(rawSet2Data)
+// const rawSet3Data = fs.readFileSync("server/Set3.json")
+// const set3Data = JSON.parse(rawSet3Data)
 
 adminsData.admins.map(record => {
     console.log(record)
@@ -17,3 +26,16 @@ adminsData.admins.map(record => {
         console.log("Admin record saved")
     })
 })
+
+set1Data.Set1.map(record => {
+    console.log(record)
+    const newSet1 = new Set1({
+        question: record.question,
+        answers: record.answers,
+        correct: record.correct
+    })
+    newSet1.save().then(result => {
+        console.log("Set1 record saved")
+    })
+})
+
---
+++ server/models/Set2.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const dotenv = require("dotenv")
+
+dotenv.config()
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+const doConnect = async (url) => {
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+      console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+      console.log('error connecting to MongoDB:', error.message)
+    })
+}
+doConnect(url)
+
+const Set2Schema = new mongoose.Schema({
+    question: String,
+    answers: Array,
+    correct: String,
+})
+
+Set2Schema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Set2 = mongoose.model("Set2", set2Schema)
+
+module.exports = Set2
