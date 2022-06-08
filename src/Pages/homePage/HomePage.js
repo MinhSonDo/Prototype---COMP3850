2454f9896d262c883c9ee6edba144d7f14d9fba1
minh son do <87711555+MinhSonDo@users.noreply.github.com>
2022-06-08 23:29:25 +1000

    Randomizer part now does not refresh

--- src/Pages/homePage/HomePage.js
+++ src/Pages/homePage/HomePage.js
@@ -1,5 +1,5 @@
 // import Menu from '../menuPage/MenuPage'
-import { Link } from 'react-router-dom'
+import { Link, RouterPrompt } from 'react-router-dom'
 
 
 
@@ -7,7 +7,10 @@ import { Link } from 'react-router-dom'
 function Button() {
 
 	return (
+
 		<div className="buttons is-centered">
+
+
 			<Link to="/login">
 				<button className="button"> Log In </button>
 			</Link>
